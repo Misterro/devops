b6091470db4f04a77882906e46fe0a865427239b
samalyshev <samalyshev@sberbank.ru>
2021-10-02 14:46:29 +0300

    playbook docker

--- build/git.sh
+++ build/git.sh
@@ -8,14 +8,6 @@ set -e -x
 [ -z "${COMMIT_EMAIL}" ] && { echo "Need to set COMMIT_EMAIL"; exit 1; }
 [ -z "${SSH_KEY}" ] && { echo "Need to set SSH_KEY"; exit 1; }
 
-if [ ! -d ~/.ssh ]; then
-	echo "SSH Key was not found. Configuring SSH Key."
-	mkdir ~/.ssh
-	echo -e "${SSH_KEY}" > ~/.ssh/id_rsa
-	chmod 700 ~/.ssh
-	chmod 600 ~/.ssh/id_rsa
-fi
-
 nano /root/.ssh/id_rsa
 
 git init
