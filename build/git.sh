fc13a73d2f737fa4b321a4479f51294c98423d6f
samalyshev <samalyshev@sberbank.ru>
2021-10-03 18:48:35 +0300

    playbook docker

--- build/git.sh
+++ build/git.sh
@@ -15,6 +15,7 @@ git config user.name "${COMMIT_USER}"
 git config user.email "${COMMIT_EMAIL}"
 git add hello-1.0.war
 git commit -m "Update detected changes."
+ls -la /root/.ssh/
 ssh -T git@github.com -vv
 git remote set-url origin git@github.com:Misterro/s.git
 git push "${GIT_ORIGIN}" "${GIT_BRANCH}"
